c8ad14ff6733044783f466761b591e6fcc5dd69a
Allie Zhao <allie.k.zhao@gmail.com>
2023-08-11 17:22:41 -0700

    Add configuration, csproj, Program.cs files

---
+++ TreatTracker/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TreatTracker.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace TreatTracker
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllersWithViews();
+
+      builder.Services.AddDbContext<TreatTrackerContext>(
+                        dbContextOptions => dbContextOptions
+                          .UseMySql(
+                            builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                          )
+                        )
+                      );
+
+      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                  .AddEntityFrameworkStores<TreatTrackerContext>()
+                  .AddDefaultTokenProviders();
+
+      WebApplication app = builder.Build();
+
+      // app.UseDeveloperExceptionPage();
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}");
+
+      app.Run();
+    }
+  }
+}
