7f12ef318030337e637474195e00501700dc64bb
Allie Zhao <allie.k.zhao@gmail.com>
2023-08-18 18:38:11 -0700

    Add treat delete route and view

--- TreatTracker/Controllers/TreatsController.cs
+++ TreatTracker/Controllers/TreatsController.cs
@@ -84,4 +84,20 @@ public class TreatsController : Controller
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    [Authorize]
+    public ActionResult Delete(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 }
